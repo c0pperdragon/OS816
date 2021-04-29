d96b4493808d980c030c65c74542941c13726051
c0pperdragon <reinhard.grafl@aon.at>
2021-04-29 21:48:36 +0200

    Get rid of the mandatory 2 bytes in the UDATA section.

--- src/startup.asm
+++ src/startup.asm
@@ -24,30 +24,39 @@ START:
     TCS 
 
     ; the DATA and UDATA segments must reside in the same bank for 
-    ; direct access. The DBR will always contain the number of this bank.
-    ; The second block copy instruction below has the side effect of also setting
+    ; direct access. The DBR will always contain the number of this bank, if any 
+    ; of these is non-empty.
+    ; Each block copy instruction below has the side effect of also setting
     ; the DBR.
 
     ; copy initial content into DATA segment 
     LDA #_END_DATA-_BEG_DATA ;number of bytes to copy
-    BEQ SKIP ;if none, just skip
+    BEQ SKIPDATA 
     DEC A ;less one for MVN instruction
     LDX #<_ROM_BEG_DATA ;get source into X
     LDY #<_BEG_DATA ;get dest into Y
     MVN #(^_ROM_BEG_DATA)+$80,#^_BEG_DATA ;copy bytes
-SKIP:
+SKIPDATA:
 
-    ; clear UDATA segment
-    LDA #0
-    STA >_BEG_UDATA   ; clear first two bytes (which must exist)
+    ; clear UDATA segment and set DBR
     LDA #_END_UDATA-_BEG_UDATA ;get total number of bytes to clear
+    BEQ SKIPUDATA 
+    ; clear first byte of buffer
+    SEP #$20 ;8 bit accu/memory
+    LONGA OFF
+      LDA #0
+      STA >_BEG_UDATA  
+    REP #$20 ;8 bit accu/memory
+    LONGA ON   
     DEC A
-    DEC A
+    BEQ SKIPUDATA ; buffer is of length 1, so no clear loop is needed
+    DEC A    
+    ; make a overlapping transfer to zero out the whole buffer
     LDX #<_BEG_UDATA ;get start of area into X
     TXY
     INY              ;get the next byte address int Y
-    MVN #^_BEG_UDATA,#^_BEG_UDATA ;zero out bytes with this overlapping block copy
-DONE: 
+    MVN #^_BEG_UDATA,#^_BEG_UDATA 
+SKIPUDATA: 
 
 ;    ; set the output port to a defined state
     LDA #$00FF
@@ -57,13 +66,6 @@ DONE:
     ; start the main function, and stop CPU upon return
     JSL >~~main
     STP
-    ENDS
-
-    ; specify a tiny UDATA section to make sure that UDATA exists in any case,
-    ; this makes the initializing code so much shorter, because it does not
-    ; need to handle any special cases
-    UDATA
-    DS 2
     ENDS
 
     ; The initial reset vector and a single long jump instruction to 
