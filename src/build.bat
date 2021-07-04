1c026aec56d9d6914b876241375aaa49065b05bf
c0pperdragon <reinhard.grafl@aon.at>
2021-07-04 11:33:38 +0200

    Changed my mind about naming the folder for the examples

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,6 @@
+del ..\bin\*.lib
+del ..\bin\*.obj
+
 WDC816AS startup.asm -O..\bin\startup.obj
 WDC816AS portio.asm
 WDC816AS sleep.asm
@@ -5,6 +8,8 @@ WDC816AS serial.asm
 WDC816CC -ML -MV -MU -MK -MT -SOP -WL -WP textio.c
 WDC816CC -A -ML -MV -MU -MK -MT -SOP -WL -WP string.c
 WDC816CC -ML -MV -MU -MK -MT -SOP -WL -WP string.c
-del ..\bin\OS816.lib
-WDCLIB -A ..\bin\OS816.lib portio.obj sleep.obj serial.obj textio.obj string.obj
+WDCLIB -A ..\bin\core.lib portio.obj sleep.obj serial.obj textio.obj string.obj
+
+WDC816AS os816.asm -O..\bin\os816.obj
+
 del *.obj
